c42d1ad032fc759ece674e1f9921b85ef82fc208
ansolotli <annaliinakoskinen@gmail.com>
2018-04-14 17:02:26 +0300

    lisää kaloja + värit

--- Fishquest/src/main/java/fishquest/gui/GameApplication.java
+++ Fishquest/src/main/java/fishquest/gui/GameApplication.java
@@ -1,4 +1,3 @@
-
 package fishquest.gui;
 
 import fishquest.logics.Boat;
@@ -16,60 +15,57 @@ import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class GameApplication extends Application {
-    
+
     @Override
     public void init() {
         //tänne tietokannan alustus, daot yms.
     }
-    
+
     public static int WIDTH = 800;
     public static int HEIGHT = 400;
-    
+
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Catch the fish!");
         Pane layout = new Pane();
         layout.setPrefSize(WIDTH, HEIGHT);
         Scene scene = new Scene(layout);
-        
-        Boat boat = new Boat(WIDTH/2, HEIGHT/2);
+
+        Boat boat = new Boat(WIDTH / 2, HEIGHT / 2);
         layout.getChildren().add(boat.getShape());
-        
+
         List<Fish> listOfFish = new ArrayList<>();
         for (int i = 0; i < 10; i++) { //10 kalaa testimäärä
             Random random = new Random();
             Fish fish = new Fish(random.nextInt(WIDTH / 3), random.nextInt(HEIGHT));
-            //fish.setFill(Color.DARKBLUE);
             listOfFish.add(fish);
         }
-        
+
         //randomoi kalojen luominen
-        
         listOfFish.forEach(fish -> layout.getChildren().add(fish.getShape()));
-        
+
         //luo karit
-        
         Map<KeyCode, Boolean> keysPressed = new HashMap<>();
-        
+
         scene.setOnKeyPressed(e -> {
             keysPressed.put(e.getCode(), Boolean.TRUE);
         });
         scene.setOnKeyReleased(e -> {
             keysPressed.put(e.getCode(), Boolean.FALSE);
         });
-        
+
         new AnimationTimer() {
-            
+
             private long lastUpdate = 0;
-            
+
             @Override
             public void handle(long now) {
-                
+
                 if (now - lastUpdate < 8000000) {
                     return;
                 }
                 lastUpdate = now;
-                
+
                 if (keysPressed.getOrDefault(KeyCode.LEFT, false)) {
                     boat.left();
                 }
@@ -82,35 +78,43 @@ public class GameApplication extends Application {
                 if (keysPressed.getOrDefault(KeyCode.DOWN, false)) {
                     boat.down();
                 }
-                
+
                 boat.move();
                 listOfFish.forEach(fish -> fish.move());
-                
+
                 listOfFish.forEach(fish -> {
-                   if (boat.collidesWith(fish)) {
-                       layout.getChildren().remove(fish.getShape());
-                       //poista napattu kala listalta
-                       //piste pelaajalle
-                   }
+                    if (boat.collidesWith(fish)) {
+                        layout.getChildren().remove(fish.getShape());
+                        //poista napattu kala listalta
+                        //piste pelaajalle
+                    }
                 });
+
+                if (Math.random() < 0.010) {
+                    Random random = new Random();
+                    Fish fish = new Fish(random.nextInt(WIDTH / 4), random.nextInt(HEIGHT));
+                    if (!fish.collidesWith(boat)) {
+                        listOfFish.add(fish);
+                        layout.getChildren().add(fish.getShape());
+                    }
+                }
             }
         }.start();
-        
+
 //        stage.setOnCloseRequest(e -> {
 //            e.consume();
 //            stop();
 //        });
-        
         stage.setScene(scene);
         stage.show();
     }
-    
+
     @Override
     public void stop() {
         //tietokannan tallennus
         //ohjelman sulkeminen
     }
-    
+
     public static void main(String[] args) {
         launch(args);
     }
--- Fishquest/src/main/java/fishquest/logics/Fish.java
+++ Fishquest/src/main/java/fishquest/logics/Fish.java
@@ -1,7 +1,9 @@
 
 package fishquest.logics;
 
+import java.util.Random;
 import javafx.geometry.Point2D;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
 
 public class Fish extends MovingShape {
@@ -9,5 +11,14 @@ public class Fish extends MovingShape {
     public Fish(int x, int y) {
         super(new Polygon(0, 0, 0, 20, 20, 10, 40, 0, 60, 10, 40, 20), x, y);
         super.motion = new Point2D(1, 0);
+        super.shape.setFill(generateRandomColor());
+    }
+    
+    public Color generateRandomColor() {
+        Random random = new Random();
+        int r = random.nextInt(255);
+        int g = random.nextInt(255);
+        int b = random.nextInt(255);
+        return Color.rgb(r, g, b);
     }
 }
